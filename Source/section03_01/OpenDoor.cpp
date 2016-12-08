3cee6bc9d9ebd8afc30cf0de7299d689cc99764a
nghia <nghiank@hotmail.com>
2016-12-07 20:26:31 -0800

    Add first level

--- Source/section03_01/OpenDoor.cpp
+++ Source/section03_01/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "section03_01.h"
-#include "PositionReport.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,20 +16,18 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	FString objectName= GetOwner()->GetName();
-	FTransform t = GetOwner()->GetTransform();
-	FString position = t.ToHumanReadableString();
-	UE_LOG(LogTemp, Warning, TEXT("Position reporting for duty! %s %s"), *objectName, *position);
-	// ...
-	
+
+	AActor* owner = GetOwner();
+	FRotator rotator(0.f, 60.f, 0.f);
+	owner->SetActorRotation(rotator);
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
